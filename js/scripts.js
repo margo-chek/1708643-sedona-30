c99ab167a1867bc0d210b0ab8186b445db5d05ed
Margo <mnchek@mail.ru>
2021-03-18 23:29:17 +0300

    оживление интерфейса

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+const searchForm  = document.querySelector(".search-hostel-button");
+const search = document.querySelector(".search-form");
+const form = search.querySelector("form");
+const dateIn = search.querySelector("[name = date-in]");
+const dateOut = search.querySelector("[name = date-out]");
+const adult = search.querySelector("[name = adult]");
+
+searchForm.addEventListener("click", function(e) {
+  e.preventDefault();
+  if (search.classList.contains("form-show")) {
+    search.classList.remove("form-show");
+  } else {
+    search.classList.add("form-show");
+    dateIn.focus();
+  }
+});
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  if (!dateIn.value || !dateOut.value || !adult.value) {
+    alert("Заполните обязательные поля");
+  }
+})
+
+window.addEventListener("keydown", function(e) {
+  if (e.keyCode === 27) {
+    if (search.classList.contains("form-show")) {
+      e.preventDefault();
+      search.classList.remove("form-show");
+    }
+  }
+})
